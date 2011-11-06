8773144f7dd13f005765d4dd3fe8625c347cb249
Sunil Pedapudi <skpedapudi@gmail.com>
2011-11-06 03:16:21 -0800

    Add: parsing of pianobar config file Add: hook into runtime from remote such that remote doesn't use subprocess.call Add: more TODO items Add: UDP_IP and UDP_PORT as command line parameters

--- remote.py
+++ remote.py
@@ -8,47 +8,56 @@
 # Nov. 5, 2011
 #
 # [1] https://github.com/PromyLOPh/pianobar
+# TODO: add better default behavior so this doesn't break on machines
+# other than my own.
+# (1) parameterize 
+#   (a) udp port
+#   (b) fifo
+#   (c) config file
+# (2) figure out default behavior for lack of config
+#   (a) prompt user?
+# (3) figure out default behavior if ctl is missing
+#   (a) mkfifo?
+# (4) what are failsafe defaults for keybindings?
+#   (a) make a config file?
+# (5) create an install.sh that makes fifo and config?
+# (6) embed event_cmd behavior into this script (somehow)
 
 import atexit, multiprocessing, os, socket, subprocess, sys
-UDP_IP="127.0.0.1" # run on localhost
-UDP_PORT=64266 # PIANO on T9 - 10000
 
-# TODO: fix this massive hack by introducing a schema and packets
-# that understand pianobar's commands. try reading the config
-# file.
-COMMANDS={
-    "next": ["echo", "-ne", "n"],
-    "playpause":["echo", "-ne", "p"],
-    }
+# set to public ip if provided; else, use localhost
+UDP_IP = sys.argv[1] if sys.argv[1] else "127.0.0.1"
+# use specified port unless missing; then, use arbitrary default
+UDP_PORT = int(sys.argv[2]) if sys.argv[2] else 64266 # PIANO on T9 - 10000
 
 # PRLRuntime provides an overarching class that encapsulates the
 # environment for all PianobarRemoteListeners. PRLRuntime takes
 # ownership of the pianobar process. 
 class PRLRuntime():
     def __init__(self):
-        self.listeners = []
+        self.inactive_listeners = []
+        self.pool = []
         atexit.register(self.terminate)
 
     def start_pianobar(self):
         # note: always initialize pianobar before fifo
         self.pianobar = subprocess.Popen("pianobar", stdout = subprocess.PIPE)
         self.fifo = open("/home/sunil/.config/pianobar/ctl", "w")
-        #TODO: figure out how to use default station on pianobar
-        subprocess.call(["echo", "-e", "20\n"], stdout=self.fifo)
+        #subprocess.call(["echo", "-e", "20\n"], stdout=self.fifo)
 
     # add listeners that can control current pianobar instance
-    # TODO: change this to dynamically add listeners
     def register_listener(self, listener):
-        self.listeners.append(listener)
+        self.inactive_listeners.append(listener)
 
     # start a separate process per listener
     def start_listeners(self):
         #TODO: name the processes
-        self.pool = [multiprocessing.Process(target=l.listen)
-                     for l in self.listeners]
-        for p in self.pool:
+        new_pool = [multiprocessing.Process(target=l.listen)
+                     for l in self.inactive_listeners]
+        for p in new_pool:
             p.daemon = True
             p.start()
+        self.pool += new_pool
 
     # clean up all running processes and open files as well as kill pianobar
     def terminate(self):
@@ -57,11 +66,14 @@ class PRLRuntime():
         self.fifo.close()
         self.pianobar.kill()
 
+    def handle(self, command):
+        subprocess.call(command(), stdout=self.fifo)
+
 # PianobarRemoteListener is intended to run in its own thread. It accepts UDP
 # packets describing which action to take on pianobar in a blocking fashion.
 class PianobarRemoteListener():
-    def __init__(self, fifo, udp_ip, udp_port):
-        self.fifo = fifo
+    def __init__(self, rt, udp_ip, udp_port):
+        self.runtime = rt
         self.udp_ip = udp_ip
         self.udp_port = udp_port
         
@@ -71,15 +83,33 @@ class PianobarRemoteListener():
         self.sock.bind((self.udp_ip, self.udp_port))
         while True:
             data, addr = self.sock.recvfrom(1024) # buffer size of 1024 bytes
-            # TODO: explore architecture: pass command up to runtime instead of
-            # interacting directly with fifo?
-            subprocess.call(COMMANDS[data], stdout=self.fifo) # giant hack
+            self.runtime.handle(lambda: COMMANDS[data])
 
+# TODO: add support for complicated commands like act_stationcreate
+# TODO: parameterize config file
+def initialize_commands():
+    # small hack that allows assignment in anonymous functions
+    def set(dct, k, v):
+        dct[k] = v
+
+    import ConfigParser
+    parser = ConfigParser.SafeConfigParser()
+    parser.read(["/home/sunil/.config/pianobar/config"])
+    
+    comm = {}
+    map(lambda kv: set(comm, kv[0],["echo", "-ne", kv[1]]),
+                       parser.items("Keybindings"))
+    
+    global COMMANDS    
+    COMMANDS = comm    
+    
 if __name__ == "__main__":
+    initialize_commands()
+    
     runtime = PRLRuntime()
     runtime.start_pianobar()
 
-    listener = PianobarRemoteListener(runtime.fifo, UDP_IP, UDP_PORT)
+    listener = PianobarRemoteListener(runtime, UDP_IP, UDP_PORT)
 
     runtime.register_listener(listener)
     runtime.start_listeners()
@@ -89,6 +119,6 @@ if __name__ == "__main__":
     while(True): 
         # TODO: figure out how to parse pianobar event_cmd. 
         # and display that as output
-        # (line 393 of pianobar/ui.h)
+        # (line 393 of /path/to/pianobar/ui.h)
         sleep(60)
     
